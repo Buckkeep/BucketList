6f26e3dc56f251d6e625cfb85c36d25ce685384e
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-05 15:16:11 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
